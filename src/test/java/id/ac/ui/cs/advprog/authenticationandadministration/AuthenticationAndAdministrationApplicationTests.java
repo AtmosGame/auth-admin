14ccd8f472d17eea63e53e8a63e377b4b913ffae
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-04-01 15:22:56 +0700

    Init project

---
+++ src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.authenticationandadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
