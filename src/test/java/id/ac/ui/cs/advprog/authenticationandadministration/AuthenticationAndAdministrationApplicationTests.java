c7ac03ef0f859cb624a4a0cf3ff426c99a600202
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-04-06 22:35:35 +0700

    Change time zone in exception handler

--- src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
@@ -1,13 +1,12 @@
 package id.ac.ui.cs.advprog.authenticationandadministration;
 
-import id.ac.ui.cs.advprog.authenticationandadministration.controller.AuthController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 class AuthenticationAndAdministrationApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 }
