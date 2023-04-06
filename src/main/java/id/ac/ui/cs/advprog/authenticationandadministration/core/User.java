6a310d9df624499fd43d35c87d9291b41950c20c
eugeniusms <eugeniusms@gmail.com>
2023-04-06 14:15:15 +0700

    🛠️ Add ID (PK) to User

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/core/User.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/core/User.java
@@ -1,14 +1,23 @@
 package id.ac.ui.cs.advprog.authenticationandadministration.core;
 
 public class User {
+    private static int total = 0;
+    private final int id;
     private final String username;
     private final String password;
     private final String role;
     public User(String username, String password, String role) {
+        this.id = generateId();
         this.username = username;
         this.password = password;
         this.role = role;
     }
+
+    public int generateId() {
+        return total++;
+    }
+
+    public int getId() { return id; }
 
     public String getPassword() {
         return password;
