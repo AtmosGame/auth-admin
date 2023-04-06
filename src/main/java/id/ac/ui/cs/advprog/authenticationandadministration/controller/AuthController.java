7d9a6bea86d15fbd293723339110f88078edcee0
eugeniusms <eugeniusms@gmail.com>
2023-04-06 13:32:42 +0700

    🔨 Add Dummy JSON Response Page

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/controller/AuthController.java
@@ -2,12 +2,17 @@ package id.ac.ui.cs.advprog.authenticationandadministration.controller;
 
 import id.ac.ui.cs.advprog.authenticationandadministration.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/auth")
@@ -16,14 +21,23 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @GetMapping(path = "/register")
+    public String registerPage(Model model) {
+        return "auth/register";
+    }
+
     @GetMapping(path = "/login")
     public String loginPage(Model model) {
         return "auth/login";
     }
 
-    @GetMapping(path = "/register")
-    public String registerPage(Model model) {
-        return "auth/register";
+    @PostMapping(path = "/register")
+    public String register(Model model,
+                           @RequestParam(value = "username") String username,
+                           @RequestParam(value = "password") String password,
+                           @RequestParam(value = "role") String role) {
+        authService.register(username, password, role);
+        return "redirect:/auth/login";
     }
 
     @PostMapping(path = "/login")
@@ -38,13 +52,11 @@ public class AuthController {
         return "auth/login";
     }
 
-    @PostMapping(path = "/register")
-    public String register(Model model,
-                           @RequestParam(value = "username") String username,
-                           @RequestParam(value = "password") String password,
-                           @RequestParam(value = "role") String role) {
-        authService.register(username, password, role);
-        return "redirect:/auth/login";
+    @GetMapping("/users")
+    public ResponseEntity<Object> getData() {
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        data.put("key2", "value2");
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
-
 }
