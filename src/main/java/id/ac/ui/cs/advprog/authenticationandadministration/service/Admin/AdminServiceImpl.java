69194c6ec8aebb4696a8a261375e306004292efb
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-04-06 23:00:03 +0700

    Complete the use case (not including unit tests)

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/Admin/AdminServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/Admin/AdminServiceImpl.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.authenticationandadministration.service.Admin;
 
 import id.ac.ui.cs.advprog.authenticationandadministration.dto.ViewProfileResponse;
 import id.ac.ui.cs.advprog.authenticationandadministration.exceptions.UserDoesNotExistException;
+import id.ac.ui.cs.advprog.authenticationandadministration.exceptions.UserHasBeenBlockedException;
 import id.ac.ui.cs.advprog.authenticationandadministration.exceptions.UserIsAdministratorException;
 import id.ac.ui.cs.advprog.authenticationandadministration.models.User;
 import id.ac.ui.cs.advprog.authenticationandadministration.repository.UserRepository;
@@ -17,22 +18,24 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public ViewProfileResponse getUserByUsername(String username) {
-        if (isUserDoesNotExist(username)){
+        if (userRepository.findByUsername(username).isEmpty()){
             throw new UserDoesNotExistException(username);
         }
 
         User user = userRepository.findByUsername(username).get();
-
-        if (user.getRole().equals("Administrator")){
-            throw new UserIsAdministratorException(username);
-        }
-
+        userValidation(user);
         return ViewProfileResponse.builder().username(user.getUsername())
                 .role(user.getRole()).profilePicture(user.getProfilePicture())
                 .bio(user.getBio()).applications(user.getApplications()).build();
     }
 
-    private boolean isUserDoesNotExist(String username){
-        return userRepository.findByUsername(username).isEmpty();
+    private void userValidation(User user){
+        if (user.getRole().equals("administrator")){
+            throw new UserIsAdministratorException(user.getUsername());
+        }
+
+        if (!user.getActive()){
+            throw new UserHasBeenBlockedException(user.getUsername());
+        }
     }
 }
