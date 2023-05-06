3b1b7f24f25f23e4280aabffd8d050cafae0501c
Kenneth Ferdinand <kenneth.ferdinand@ui.ac.id>
2023-05-06 10:56:46 +0700

    done search users

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/user/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/user/UserService.java
@@ -1,9 +1,16 @@
 package id.ac.ui.cs.advprog.authenticationandadministration.service.user;
 
 import id.ac.ui.cs.advprog.authenticationandadministration.dto.user.CurrentUserResponse;
+import id.ac.ui.cs.advprog.authenticationandadministration.dto.user.SearchUserRequest;
+import id.ac.ui.cs.advprog.authenticationandadministration.models.auth.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public interface UserService {
     CurrentUserResponse getCurrentUser(String username);
+
+    List<User> searchUsers(SearchUserRequest request);
 }
