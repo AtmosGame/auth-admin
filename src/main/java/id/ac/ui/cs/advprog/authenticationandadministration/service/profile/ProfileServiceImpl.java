602477bbae2c7f94ddf52885374edda4b1df3dcf
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2023-05-10 21:14:37 +0700

    profile picture using cloudinary (not fixed)

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/profile/ProfileServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/profile/ProfileServiceImpl.java
@@ -1,11 +1,16 @@
 package id.ac.ui.cs.advprog.authenticationandadministration.service.profile;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import id.ac.ui.cs.advprog.authenticationandadministration.dto.profile.ViewProfileResponse;
 import id.ac.ui.cs.advprog.authenticationandadministration.models.auth.User;
 import id.ac.ui.cs.advprog.authenticationandadministration.repository.UserRepository;
 import id.ac.ui.cs.advprog.authenticationandadministration.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +34,21 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public User updateProfile(String username, String bio, String profilePicture){
         User user = userService.getUserNonAdminByUsername(username);
+        // Check if the profile picture is not empty
+        if (!profilePicture.isEmpty()) {
+            try {
+                // Upload the image to Cloudinary and get the public URL
+                Cloudinary cloudinary = new Cloudinary();
+                Map uploadResult = cloudinary.uploader().upload(profilePicture, ObjectUtils.emptyMap());
+                String imageUrl = (String) uploadResult.get("url");
 
+                // Set the profile picture URL in the user object
+                user.setProfilePicture(imageUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         user.setBio(bio);
-        user.setProfilePicture(profilePicture);
         return userRepository.save(user);
     }
 }
