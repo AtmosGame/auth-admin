31188c8c7130873debca9d6d8e59564c280c9e5e
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-05-15 20:24:40 +0700

    Add several unit test & change several logic

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/controller/ReportController.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/controller/ReportController.java
@@ -7,6 +7,7 @@ import id.ac.ui.cs.advprog.authenticationandadministration.service.report.Report
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,6 +27,7 @@ public class ReportController {
 //    }
 
     @GetMapping("/reported-account")
+    @PreAuthorize("hasAuthority('user:read')")
     public ResponseEntity<ReportedAccountResponse> getAllReportedAccount(){
         return new ResponseEntity<>(reportService.getAllReportedAccount(), HttpStatus.OK);
     }
