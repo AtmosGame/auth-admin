8935946ff0bff5ae35853a60c3271647b7425fc7
eugeniusms <eugeniusms@gmail.com>
2023-05-16 03:08:55 +0700

    🔧 JwtServiceTest Done (76% Coverage)

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/auth/JwtService.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/service/auth/JwtService.java
@@ -58,7 +58,7 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    private Claims extractAllClaims(String token) {
+    public Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignInKey())
