ce08333e25c558ba6050822b02f5ca7b16092c65
eugeniusms <eugeniusms@gmail.com>
2023-05-20 21:32:06 +0700

    🔧 Fix Security Config for Search User

--- src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/core/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/authenticationandadministration/core/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/v1/auth/**", "/v1/profile/view-profile/**", "/v1/user/search-user")
+                .requestMatchers("/v1/auth/**", "/v1/profile/view-profile/**", "/v1/user/search-user/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
