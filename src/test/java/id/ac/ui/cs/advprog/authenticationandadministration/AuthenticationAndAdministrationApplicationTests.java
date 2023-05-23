6bafa090b3c984f6f6fdbedbf0e557fe8c798b36
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-05-24 04:02:21 +0700

    Fix: Error

--- src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
@@ -1,18 +1,7 @@
 package id.ac.ui.cs.advprog.authenticationandadministration;
 
-import id.ac.ui.cs.advprog.authenticationandadministration.controller.UserController;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthenticationAndAdministrationApplicationTests {
-    @Autowired
-    private UserController userController;
-
-    @Test
-    void contextLoads() {
-        Assertions.assertThat(userController).isNotNull();
-    }
 }
