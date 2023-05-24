762813a5d07e6aa01af80039016521953a1db694
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-05-25 01:52:46 +0700

    Try to fix error #3

--- src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
@@ -1,19 +1,7 @@
 package id.ac.ui.cs.advprog.authenticationandadministration;
 
-import id.ac.ui.cs.advprog.authenticationandadministration.controller.AuthController;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AuthenticationAndAdministrationApplicationTests {
-    @Autowired
-    private AuthController authController;
-
-    @Test
-    void contextLoads() {
-        assertThat(authController).isNotNull();
-    }
 }
