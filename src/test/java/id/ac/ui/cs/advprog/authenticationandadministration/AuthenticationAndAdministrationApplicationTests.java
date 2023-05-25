7369d0210181d49cfea4767b09fe811b5119a6f7
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-05-25 08:32:38 +0700

    Add application test

--- src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/authenticationandadministration/AuthenticationAndAdministrationApplicationTests.java
@@ -1,7 +1,18 @@
 package id.ac.ui.cs.advprog.authenticationandadministration;
 
+import id.ac.ui.cs.advprog.authenticationandadministration.controller.AuthController;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthenticationAndAdministrationApplicationTests {
+    @Autowired
+    private AuthController authController;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(authController).isNotNull();
+    }
 }
